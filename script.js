5d9147ff4d80d3b709909bad097044d78a68d8de
Rishisingare108 <singarerishi108@gmail.com>
2025-09-14 20:06:53 +0530

    all done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("hi")
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
